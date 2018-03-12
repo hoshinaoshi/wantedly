e4d664075f05c875e232593582cb630b29117166
Ayumi Tamai <ayumi@Ayumis-MBP.local>
2018-03-13 02:30:51 +0900

    mechanizeでログイン後nokogiriでスクレイピングしようとした

--- wantedly.rb
+++ wantedly.rb
@@ -1,23 +1,26 @@
 require "mechanize"
+require "open-uri"
 require "net/http"
 require "nokogiri"
 
 agent = Mechanize.new
 agent.user_agent = "Windows Mozilla"
 
-agent.get('https://www.wantedly.com/user/sign_in') do |page|
-  response = page.form_with(:dom_id => 'new_user') do |form|
+agent.get('https://www.wantedly.com/user/sign_in') do |loginpage|
+  response = loginpage.form_with(:dom_id => 'new_user') do |form|
     form.field_with(:name => 'user[email]').value = ARGV[0]
     form.field_with(:name => 'user[password]').value = ARGV[1]
   end.submit
   p "submitted"
 
-  url = "https://www.wantedly.com/enterprise/scouts#search%5Bkeywords%5D=&search%5Boccupation_types%5D%5B%5D=engineer&search%5Bactivity%5D=7&search%5Blocations%5D%5B%5D=kanto&search%5Bmotivation%5D=large&search%5Bscout_reply%5D=&search%5Bconnection%5D=&search%5Bscout_received%5D=&search%5Bage_range%5D=18-35&search%5Bgraduation_year%5D=&search%5Border%5D=recommend&search%5Bcountries%5D%5B%5D=japan&search%5Brecommended%5D=false&search%5Bbookmarked_users_params%5D=&filter_is_used=true"
+  page = agent.get("https://www.wantedly.com/enterprise/scouts#search%5Bkeywords%5D=&search%5Boccupation_types%5D%5B%5D=engineer&search%5Bactivity%5D=7&search%5Blocations%5D%5B%5D=kanto&search%5Bmotivation%5D=large&search%5Bscout_reply%5D=&search%5Bconnection%5D=&search%5Bscout_received%5D=&search%5Bage_range%5D=18-35&search%5Bgraduation_year%5D=&search%5Border%5D=recommend&search%5Bcountries%5D%5B%5D=japan&search%5Brecommended%5D=false&search%5Bbookmarked_users_params%5D=&filter_is_used=true")
   # 検索条件をパラメータに指定
 
-  page = agent.get(url)
-  page.search('div.container').each do |obj|
-    # p obj.inner_text
+  html = Nokogiri::HTML.parse(page)
+  p html
+  # p html.css('#main-inner')
+  html.css('.user-name-wrapper').each do |usr|
+    # p usr
   end
   p "end!"
 
