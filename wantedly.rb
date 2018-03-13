9b1369f4966c6b5cdd6d0eb8c8d587551be0ee0d
玉井 あゆみ <ayumi_tamai@a-143.local>
2018-03-13 16:05:11 +0900

    .

--- wantedly.rb
+++ wantedly.rb
@@ -2,6 +2,7 @@ require "mechanize"
 require "open-uri"
 require "net/http"
 require "nokogiri"
+require "capybara"
 
 agent = Mechanize.new
 agent.user_agent = "Windows Mozilla"
@@ -13,10 +14,12 @@ agent.get('https://www.wantedly.com/user/sign_in') do |loginpage|
   end.submit
   p "submitted"
 
+
+
   agent.get("https://www.wantedly.com/enterprise/scouts#search%5Bkeywords%5D=&search%5Boccupation_types%5D%5B%5D=engineer&search%5Bactivity%5D=7&search%5Blocations%5D%5B%5D=kanto&search%5Bmotivation%5D=large&search%5Bscout_reply%5D=&search%5Bconnection%5D=&search%5Bscout_received%5D=&search%5Bage_range%5D=18-35&search%5Bgraduation_year%5D=&search%5Border%5D=recommend&search%5Bcountries%5D%5B%5D=japan&search%5Brecommended%5D=false&search%5Bbookmarked_users_params%5D=&filter_is_used=true") do |page|
 
     html = Nokogiri::HTML.parse(page.body)
-    puts html
+    puts html.inner_html
 
   end
 
